178c823ab8e07cec733f60220d8c95e22ce3859d
Dilara <dilarademircan1996@gmail.com>
2023-09-11 16:32:08 +0300

    created home activity dependent fragments and navigation graph

---
+++ app/src/main/java/com/dilara/mydiary/view/ShareFragment.kt
@@ -0,0 +1,25 @@
+package com.dilara.mydiary.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dilara.mydiary.R
+
+
+class ShareFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_share, container, false)
+    }
+
+}
