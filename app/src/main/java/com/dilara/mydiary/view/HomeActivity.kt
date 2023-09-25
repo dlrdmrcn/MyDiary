d7d63a0da3b2e1167a5ac3ab0df075b0cc339d3b
Dilara <dilarademircan1996@gmail.com>
2023-09-25 17:28:17 +0300

    toolbar and drawer menu created, bottom nav menu edited

--- app/src/main/java/com/dilara/mydiary/view/HomeActivity.kt
+++ app/src/main/java/com/dilara/mydiary/view/HomeActivity.kt
@@ -2,11 +2,92 @@ package com.dilara.mydiary.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.dilara.mydiary.R
+import com.dilara.mydiary.databinding.ActivityHomeBinding
+import com.google.android.material.navigation.NavigationView
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var binding : ActivityHomeBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var toolbar: androidx.appcompat.widget.Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        replaceFragment(HomeFragment())
+
+
+        drawerLayout = binding.drawerLayout
+        navigationView = binding.navView
+        toolbar = binding.toolbar
+
+        navigationView.setNavigationItemSelectedListener(this@HomeActivity)
+
+        toggle = ActionBarDrawerToggle(this@HomeActivity,drawerLayout,toolbar,R.string.open_nav,R.string.close_nav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment()).commit()
+            navigationView.setCheckedItem(R.id.nav_view)
+        }
+
+        binding.bottomNavMenu.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_home -> replaceFragment(HomeFragment())
+                R.id.nav_calendar -> replaceFragment(CalendarFragment())
+                R.id.nav_add -> replaceFragment(AddDiaryFragment())
+                R.id.nav_profile -> replaceFragment(ProfileFragment())
+
+                else ->{
+
+
+                }
+            }
+
+            true
+        }
+
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.drawer_export -> replaceFragment(ExportFragment())
+            R.id.drawer_import -> replaceFragment(ImportFragment())
+            R.id.drawer_donate -> replaceFragment(DonateFragment())
+            R.id.drawer_rate -> replaceFragment(RateFragment())
+            R.id.drawer_share -> replaceFragment(ShareFragment())
+            R.id.drawer_privacy_policy -> replaceFragment(PrivacyPolicyFragment())
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+    override fun onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }else{
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container,fragment)
+        fragmentTransaction.commit()
+    }
+
 }
