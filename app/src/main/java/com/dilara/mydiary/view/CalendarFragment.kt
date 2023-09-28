e2d89d75b63f0b7312e1792eb9ac9174467bf769
Dilara <dilarademircan1996@gmail.com>
2023-09-28 20:23:32 +0300

    calendar fragment design completed

--- app/src/main/java/com/dilara/mydiary/view/CalendarFragment.kt
+++ app/src/main/java/com/dilara/mydiary/view/CalendarFragment.kt
@@ -5,9 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.dilara.mydiary.R
+import android.widget.DatePicker
+import com.dilara.mydiary.MONTH
+import com.dilara.mydiary.databinding.FragmentCalendarBinding
+import java.util.Calendar
 
 class CalendarFragment : Fragment() {
+    private lateinit var binding : FragmentCalendarBinding
+    private lateinit var datePicker : DatePicker
+    private lateinit var today : Calendar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -15,8 +21,22 @@ class CalendarFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
+    ): View {
+        binding = FragmentCalendarBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        datePicker = binding.datePicker
+        today = Calendar.getInstance()
+
+        val day : Int = today.get(Calendar.DAY_OF_MONTH)
+        val month = MONTH.values().firstOrNull { it.value == today.get(Calendar.MONTH) }
+        val year : Int = today.get(Calendar.YEAR)
+
+        binding.todayDateText.setText("$day $month $year")
     }
 
 }
