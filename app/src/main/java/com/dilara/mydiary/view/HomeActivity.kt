f08d90dd727e180b28559561c12d398dd5649f89
Dilara <dilarademircan1996@gmail.com>
2023-10-04 17:47:07 +0300

    drawer layout removed and necessary arrangements have been made

--- app/src/main/java/com/dilara/mydiary/view/HomeActivity.kt
+++ app/src/main/java/com/dilara/mydiary/view/HomeActivity.kt
@@ -1,93 +1,29 @@
 package com.dilara.mydiary.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.dilara.mydiary.R
 import com.dilara.mydiary.databinding.ActivityHomeBinding
-import com.google.android.material.navigation.NavigationView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class HomeActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener {
+class HomeActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityHomeBinding
-    private lateinit var drawerLayout: DrawerLayout
-    private lateinit var navigationView: NavigationView
-    private lateinit var toggle: ActionBarDrawerToggle
-    private lateinit var toolbar: androidx.appcompat.widget.Toolbar
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        replaceFragment(HomeFragment())
 
-
-        drawerLayout = binding.drawerLayout
-        navigationView = binding.navView
-        toolbar = binding.toolbar
-
-        navigationView.setNavigationItemSelectedListener(this@HomeActivity)
-
-        toggle = ActionBarDrawerToggle(this@HomeActivity,drawerLayout,toolbar,R.string.open_nav,R.string.close_nav)
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
-
-
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment()).commit()
-            navigationView.setCheckedItem(R.id.nav_view)
-        }
-
-        binding.bottomNavMenu.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.nav_home -> replaceFragment(HomeFragment())
-                R.id.nav_calendar -> replaceFragment(CalendarFragment())
-                R.id.nav_add -> replaceFragment(AddDiaryFragment())
-                R.id.nav_profile -> replaceFragment(ProfileFragment())
-
-                else ->{
-
-
-                }
-            }
-
-            true
-        }
-
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_menu)
+        setupWithNavController(bottomNavigationView, navController)
     }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.drawer_export -> replaceFragment(ExportFragment())
-            R.id.drawer_import -> replaceFragment(ImportFragment())
-            R.id.drawer_donate -> replaceFragment(DonateFragment())
-            R.id.drawer_rate -> replaceFragment(RateFragment())
-            R.id.drawer_share -> replaceFragment(ShareFragment())
-            R.id.drawer_privacy_policy -> replaceFragment(PrivacyPolicyFragment())
-        }
-        drawerLayout.closeDrawer(GravityCompat.START)
-        return true
-    }
-    override fun onBackPressed() {
-        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
-            drawerLayout.closeDrawer(GravityCompat.START)
-        }else{
-            onBackPressedDispatcher.onBackPressed()
-        }
-
-    }
-
-    private fun replaceFragment(fragment: Fragment){
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragment_container,fragment)
-        fragmentTransaction.commit()
-    }
-
 }
--- app/src/main/java/com/dilara/mydiary/view/ProfileFragment.kt
+++ app/src/main/java/com/dilara/mydiary/view/ProfileFragment.kt
@@ -1,22 +1,22 @@
 package com.dilara.mydiary.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.dilara.mydiary.R
+import androidx.fragment.app.Fragment
+import com.dilara.mydiary.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+    private var binding: FragmentProfileBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding?.root
     }
-
 }
