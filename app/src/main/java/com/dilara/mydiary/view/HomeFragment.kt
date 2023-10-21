6889a6eb0e3c2700bc64d431c39f3fee50c6d3b3
Dilara <dilarademircan1996@gmail.com>
2023-10-21 12:07:29 +0300

    emoji counter added

--- app/src/main/java/com/dilara/mydiary/view/HomeFragment.kt
+++ app/src/main/java/com/dilara/mydiary/view/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.SavedStateViewModelFactory
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.dilara.mydiary.EMOJI
 import com.dilara.mydiary.R
 import com.dilara.mydiary.adapter.HomeRecyclerAdapter
 import com.dilara.mydiary.base.BaseFragment
@@ -48,10 +49,24 @@ class HomeFragment : BaseFragment(), HomeRecyclerAdapter.Listener {
 
     private fun initObservers() {
         viewModel.diaryLiveData.observe(requireActivity()) {
+            val emojiCountList = ArrayList<Int>()
             homeAdapter = HomeRecyclerAdapter(it, requireActivity(), requireContext(), this)
             binding?.myDiariesRecyclerView?.layoutManager = LinearLayoutManager(requireContext())
             binding?.myDiariesRecyclerView?.adapter = homeAdapter
             binding?.totalNumberDiary?.text = viewModel.diaryLiveData.value?.size.toString()
+            EMOJI.values().forEach { emoji ->
+                val count = viewModel.diaryLiveData.value?.filter {
+                    it.mood.toInt() == emoji.ordinal
+                }?.size
+                emojiCountList.add(count ?: 0)
+            }
+            binding?.numberMoodVeryhappy?.text = emojiCountList[0].toString()
+            binding?.numberMoodHappy?.text = emojiCountList[1].toString()
+            binding?.numberMoodExpressionless?.text = emojiCountList[2].toString()
+            binding?.numberMoodSad?.text = emojiCountList[3].toString()
+            binding?.numberMoodCry?.text = emojiCountList[4].toString()
+            binding?.numberMoodAngry?.text = emojiCountList[5].toString()
+            binding?.numberMoodCool?.text = emojiCountList[6].toString()
 
             homeAdapter.notifyDataSetChanged()
         }
